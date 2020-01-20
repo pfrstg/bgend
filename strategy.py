e96d8804a8a237e05140f3e8d57bcedb7aac7d3d
Patrick Riley <patriley@gmail.com>
2020-01-20 15:09:37 -0800

    Added operator overload

--- strategy.py
+++ strategy.py
@@ -9,7 +9,23 @@ class MoveCountDistribution(object):
     __slots__ = ["dist"]
     
     def __init__(self, dist=np.ones([1])):
-        self.dist = dist
+        self.dist = np.asarray(dist)
+        if len(self.dist.shape) != 1:
+            raise ValueError("Need 1D shape, got %s", self.dist.shape)
 
+    def __add__(self, other):
+        return MoveCountDistribution(self.dist + other)
+    
+    def __sub__(self, other):
+        return MoveCountDistribution(self.dist - other)
+    
+    def __mul__(self, other):
+        return MoveCountDistribution(self.dist * other)
+    
+    def __truediv__(self, other):
+        return MoveCountDistribution(self.dist / other)
+    
     def increase_counts(self, amount):
         return MoveCountDistribution(np.insert(self.dist, 0, [0] * amount))
+
+    
--- strategy_test.py
+++ strategy_test.py
@@ -10,6 +10,13 @@ class MoveCountDistributionTestCase(unittest.TestCase):
         d = strategy.MoveCountDistribution()
         np.testing.assert_allclose(d.dist, [1])
 
+    def test_operators(self):
+        d = strategy.MoveCountDistribution([0.75, 0.25])
+        np.testing.assert_allclose((d + 1).dist, [1.75, 1.25])
+        np.testing.assert_allclose((d - .05).dist, [.7, .2])
+        np.testing.assert_allclose((d * 2).dist, [1.5, .5])
+        np.testing.assert_allclose((d / 5).dist, [.15, .05])
+        
     def test_increase_counts(self):
         d = strategy.MoveCountDistribution([0.75, 0.25])
         np.testing.assert_allclose(d.increase_counts(2).dist,
