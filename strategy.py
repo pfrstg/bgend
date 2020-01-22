2504766b4d392ffd1e7fedbb5fdbeeea6ff591c9
Patrick Riley <patriley@gmail.com>
2020-01-21 21:37:24 -0800

    Add main DistributionStore code

--- strategy.py
+++ strategy.py
@@ -1,6 +1,9 @@
 # Copyright 2020 Patrick Riley <patriley@gmail.com>
 
 import numpy as np
+import time
+
+import board
 
 
 class MoveCountDistribution(object):
@@ -8,7 +11,7 @@ class MoveCountDistribution(object):
 
     __slots__ = ["dist"]
     
-    def __init__(self, dist=np.ones([1])):
+    def __init__(self, dist=np.zeros([1])):
         self.dist = np.asarray(dist)
         if len(self.dist.shape) != 1:
             raise ValueError("Need 1D shape, got %s", self.dist.shape)
@@ -42,6 +45,9 @@ class MoveCountDistribution(object):
     def __truediv__(self, other):
         """Only support multiplying with a scalar."""
         return MoveCountDistribution(self.dist / other)
+
+    def __str__(self):
+        return "MCD(%f, %s)" % (self.expected_value(), self.dist)
     
     def increase_counts(self, amount):
         return MoveCountDistribution(np.insert(self.dist, 0, [0] * amount))
@@ -52,3 +58,122 @@ class MoveCountDistribution(object):
     def expected_value(self):
         return np.sum(self.dist * range(self.dist.shape[0]))
     
+
+class DistributionStore(object):
+    """Stores MoveCountDistributions for board states."""
+
+    def __init__(self, config):
+        self.config = config
+        self.distribution_map = {}
+        
+    def get_best_moves_for_roll(self, this_board, roll):
+        """Computes the best moves for the roll.
+
+        "best" means the resulting position with the lowest expected
+        value.  "moves" means the move for every die. If there are
+        multiple groups of moves that return the same next board, one
+        will be chosen arbitrarily.
+
+        Assumes that all next board positions are already computed in
+        self.distribution_map.
+
+        Args:
+          this_board: board.Board
+          roll: board.Roll
+
+        Return
+          list of board.Move
+
+        """
+        # dict from board index to tuple of (expected_value, moves)
+        possible_next_boards = {}
+        for moves in this_board.generate_moves(roll):
+            next_board = this_board.apply_moves(moves)
+            next_board_idx = next_board.get_index()
+            if next_board_idx in possible_next_boards:
+                continue
+            possible_next_boards[next_board_idx] = (
+                self.distribution_map[next_board_idx].expected_value(),
+                moves)
+
+        best_next_board = min(possible_next_boards.keys(),
+                              key=(lambda k: possible_next_boards[k][0]))
+        
+        return possible_next_boards[best_next_board][1]
+
+    def get_move_distribution_for_board(self, this_board):
+        """Computes the MoveCountDistribution for this_board.
+
+        Assumes that all next board position are already computed in
+        self.distribution_map
+
+        Args:
+          this_board: board.Board
+
+        Return
+          MoveCountDistribution
+        """
+        out = MoveCountDistribution()
+        for roll in board.ROLLS:
+            moves = self.get_best_moves_for_roll(this_board, roll)
+            next_board = this_board.apply_moves(moves)
+            out += (self.distribution_map[next_board.get_index()]
+                    .increase_counts(1) * roll.prob)
+
+        assert out.is_normalized()
+            
+        return out
+
+    def compute(self, progress_interval=5000, limit=-1):
+        """Computes and stores MoveCountDistribution for each board.
+
+        clears an existing data in self.distribution_map
+
+        Args:
+          limit: if > 0, only computes this many valid boards
+        """
+        self.distribution_map.clear()
+
+        if progress_interval:
+            print("Starting compute on %d boards" %
+                  self.config.num_valid_boards,
+                  flush=True)
+
+        start_time = time.time()
+        # The minimum board index is the game ended state.
+        boards_processed = 1
+        self.distribution_map[self.config.min_board_index] = MoveCountDistribution([1])
+        
+        for board_idx in range(self.config.min_board_index + 1,
+                               self.config.max_board_index):
+            if not self.config.is_valid_index(board_idx):
+                continue
+
+            boards_processed += 1
+            if progress_interval and boards_processed % progress_interval == 0:
+                frac_complete = boards_processed / self.config.num_valid_boards
+                this_time = time.time()
+                print("%d/%d %.1f%%, %fs elapsed, %fs estimated total" % (
+                      boards_processed,
+                      self.config.num_valid_boards,
+                      frac_complete * 100,
+                      this_time - start_time,
+                      (this_time - start_time) / frac_complete), 
+                      flush=True)
+
+            this_board = board.Board.from_index(self.config, board_idx)
+            dist = self.get_move_distribution_for_board(this_board)
+            self.distribution_map[board_idx] = dist
+
+            if limit > 0 and boards_processed >= limit:
+                print("Stopping at %d boards, index %d"
+                      % (boards_processed, board_idx))
+                break
+
+    def pretty_string(self, limit=-1):
+        num_printed = 0
+        for board_idx, dist in self.distribution_map.items():
+            this_board = board.Board.from_index(self.config, board_idx)
+            print("Board %d" % board_idx)
+            print(dist)
+            print(this_board.pretty_string())
